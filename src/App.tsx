1949ba0592b90174b990dd6b5e33f15485675e79
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-09-24 17:42:10 -0300

    improving application using interface extensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { Post, PostType } from "./components/Post"
 import { Sidebar } from "./components/Sidebar"
 
 import styles from "./App.module.css"
+
 import "./global.css"
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
-      avatarUrl: "https://github.com/caio-guimaraes-web.png",
-      name: "Caio Guimarães",
-      role: "Front-end Developer",
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
     },
     content: [
       { type: "paragraph", content: "Fala galera 👋" },
@@ -22,7 +23,7 @@ const posts = [
       },
       { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date("2024-09-17 20:00:00"),
+    publishedAt: new Date("2022-05-03 20:00:00"),
   },
   {
     id: 2,
@@ -40,7 +41,7 @@ const posts = [
       },
       { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date("2024-09-16 20:00:00"),
+    publishedAt: new Date("2022-05-10 20:00:00"),
   },
 ]
 
@@ -53,14 +54,7 @@ export function App() {
         <Sidebar />
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            )
+            return <Post key={post.id} post={post} />
           })}
         </main>
       </div>
